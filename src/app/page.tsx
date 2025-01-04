7fbd3c0723e4cbab4c2939962b389a4b2dbfb757
Svobodennn <melih.sarac@hotmail.com>
2025-01-04 18:17:56 +0300

    Live

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,6 +163,31 @@ export default function Page() {
           ))}
         </div>
       </section>
+      <section id="projects">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <h2 className="text-xl font-bold">Projects</h2>
+          </BlurFade>
+          {DATA.projects.map((project, id) => (
+            <BlurFade
+              key={project.title}
+              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+            >
+              <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  dates={project.dates}
+                  tags={project.technologies}
+                  image={project.image}
+                  video={project.video}
+                  links={project.links}
+                />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
       {/* <section id="projects">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -24,7 +24,6 @@ export const DATA = {
     "Node.js",
     "JQuery",
     "LLM & AI Assistants",
-    "Wordpress Plugins",
     "SEO",
     "Debugging Tools",
   ],
@@ -95,18 +94,6 @@ export const DATA = {
 
   work: [
     {
-      company: "Boostarea",
-      href: "https://boostarea.net",
-      badges: ['Part Time'],
-      location: "Remote",
-      title: "Fullstack Developer",
-      logoUrl: "/boostarea.png",
-      start: "Nov 2024",
-      end: "-",
-      description:
-        "Devops, Payment system, Saas, Zendframefork",
-    },
-    {
       company: "Turkticaret.Net",
       href: "https://turkticaret.net",
       badges: ['Full Time'],
@@ -114,7 +101,7 @@ export const DATA = {
       title: "Backend Developer",
       logoUrl: "/turkticaretnet-logo.jpeg",
       start: "Jan 2024",
-      end: "-",
+      end: "Current",
       description:
         "I spearheaded operational efficiency by automating daily tasks, then boosted ***website traffic*** through ***SEO and speed optimization***. I implemented robust ***security measures***, particularly in ***payment processing***, and optimized ***server performance*** by ***query improvements***. Played a key role in implementing an ***AI Assistant*** to system to enhance customer experience",
     },
@@ -171,21 +158,22 @@ export const DATA = {
   ],
   projects: [
     {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
+      title: "Boostarea",
+      href: "https://boostarea.net",
+      dates: "Nov 2024 - Current",
       active: true,
       description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+        "Boostarea is a service that provides boost services for online games. I took part in the development of the website.",
       technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
+        "PHP",
+        "JQuery",
+        "Bootstrap 5",
+        "Zend Framework",
+        "Websocket",
+        "MySQL",
+        "SEO",
+        "Payment System",
+        "Devops",
       ],
       links: [
         {
@@ -194,103 +182,9 @@ export const DATA = {
           icon: <Icons.globe className="size-3" />,
         },
       ],
-      image: "",
+      image: "/boostarea-carousel.png",
       video:
         "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
-    {
-      title: "Magic UI",
-      href: "https://magicui.design",
-      dates: "June 2023 - Present",
-      active: true,
-      description:
-        "Designed, developed and sold animated UI components for developers.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://magicui.design",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/magicuidesign/magicui",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
-    },
-    {
-      title: "llm.report",
-      href: "https://llm.report",
-      dates: "April 2023 - September 2023",
-      active: true,
-      description:
-        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://llm.report",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/dillionverma/llm.report",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.llm.report/openai-demo.mp4",
-    },
-    {
-      title: "Automatic Chat",
-      href: "https://automatic.chat",
-      dates: "April 2023 - March 2024",
-      active: true,
-      description:
-        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://automatic.chat",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
     },
   ],
   hackathons: [
